87bcf108b0132ba452c4edcdbb0fb5139705e1a6
yarenbenhur <benhur19@itu.edu.tr>
2022-06-21 10:16:39 +0300

    Add project files.

---
+++ FanoutConsumer/ConsumerProgram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace FanoutConsumer
+{
+     class ConsumerProgram
+    {
+        static IConnection conn;
+        static IModel channel;
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                VirtualHost = "/",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+             conn = factory.CreateConnection();
+             channel = conn.CreateModel();
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += Consumer_Received;
+
+          
+            var consumerTag = channel.BasicConsume("myqueue1", false, consumer); //auto act false
+            Console.WriteLine("Waiting for messages. Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            string message = Encoding.UTF8.GetString(e.Body);
+            Console.WriteLine("Message:"+ message);
+
+            //channel.BasicAck(e.DeliveryTag, false); //giving act for received massage 
+            channel.BasicNack(e.DeliveryTag, false,true); //rejection senerio, requeue=true  
+
+            
+
+        }
+    }
+}
+
---
+++ FanoutPublisher/PublisherProgram.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace FanoutPublisher
+{
+    class PublisherProgram
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory()
+            {
+                HostName = "localhost",
+                VirtualHost = "/",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest"
+            };
+            IConnection conn = factory.CreateConnection();
+            IModel chanel = conn.CreateModel();
+
+            chanel.ExchangeDeclare("ex.fanout", "fanout", true, false, null);
+
+            chanel.QueueDeclare("myqueue1", true, false, false, null);
+            chanel.QueueDeclare("myqueue2", true, false, false, null);
+
+            chanel.QueueBind("myqueue1", "ex.fanout", "");
+            chanel.QueueBind("myqueue2", "ex.fanout", "");
+
+            chanel.BasicPublish("ex.fanout","",null,Encoding.UTF8.GetBytes("Message 1"));
+            chanel.BasicPublish("ex.fanout", "", null, Encoding.UTF8.GetBytes("Message 2"));
+
+            Console.WriteLine("Press a key to exit.");
+            Console.ReadKey();
+
+            chanel.QueueDelete("my.queue1");
+            chanel.QueueDelete("my.queue2");
+            chanel.ExchangeDelete("ex.fanout");
+
+            chanel.Close();
+            conn.Close();
+
+        }
+    }
+}
